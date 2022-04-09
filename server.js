25a7fa90811dac6aa50a5e08c9aa9fd9daa13a3e
yeonsoo888 <yeonsoo9846@gmail.com>
2022-04-09 17:00:45 +0900

    write page 생성/url 연결

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ app.listen(8080, function() {
     console.log('listening on 8080');
 });
 
-app.get('/pet',(req,res) => {
-    res.send("<h4>펫용품 쇼핑할 수 있는 사이트 입니다.</h4>");
-})
-
-app.get('/beauty',(req,res) => {
-    res.send("<h4>뷰티용품을 쇼핑할 수 있는 사이트 입니다.</h4>");
-})
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.get('/write',(req,res) => {
+    res.sendFile(__dirname + "/write.html");
 })
