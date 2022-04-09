467c775428e855055de79e5c7f667e8c4ae5abab
yeonsoo888 <yeonsoo9846@gmail.com>
2022-04-09 17:16:43 +0900

    bodyparser 설치중

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded)
 
 app.listen(8080, function() {
     console.log('listening on 8080');
@@ -13,3 +15,7 @@ app.get('/',(req,res) => {
 app.get('/write',(req,res) => {
     res.sendFile(__dirname + "/write.html");
 })
+
+app.post('/add', function(req, res){
+    res.send('전송완료')
+});
