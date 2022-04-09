d8ae61d6cc5203fdd60efe9001112035c8a9a905
yeonsoo888 <yeonsoo9846@gmail.com>
2022-04-09 16:40:41 +0900

    html파일 전송

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.get('/pet',(req,res) => {
 app.get('/beauty',(req,res) => {
     res.send("<h4>뷰티용품을 쇼핑할 수 있는 사이트 입니다.</h4>");
 })
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html");
+})
