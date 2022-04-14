5e9b7044a9edd40ccca43b88dada6fee9bcf22c1
yeonsoo888 <yeonsoo9846@gmail.com>
2022-04-14 22:48:31 +0900

    db에 데이터 입력시 id입력, 갯수 새기

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
-
 const MongoClient = require('mongodb').MongoClient;
+
+let db;
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.rtid5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',(에러,client)=>{
+    
+    if(에러) return console.log(에러);
+
+    db = client.db('todoapp');
+
+    
+
     app.listen(8080, function() {
         console.log('listening on 8080');
     });
@@ -21,7 +29,16 @@ app.get('/write',(req,res) => {
     res.sendFile(__dirname + "/write.html");
 })
 
+
 app.post('/add', function(req, res){
     res.send('전송완료');
-    console.log(req.body.title);
+
+    db.collection('counter').findOne({name: "게시물갯수"}, (err, result) => {
+        const totalLength = result.totalPost;
+
+        db.collection('post').insertOne({_id:totalLength , 할일 : req.body.title, 날짜 : req.body.date},(에러,결과) => {
+            console.log("저장완료");
+        });
+    });
+
 });
